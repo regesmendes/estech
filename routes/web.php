b697ffcceb7f9d3bb0ed5d1360ae863355b2f7c3
Reges Mendes <reges.mendes@gmail.com>
2021-02-16 21:13:30 +0000

    code cleaning

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('{any}', function () {
     return view('welcome');
 })->where('any', '.*');
-// Route::get('/', function () {
-//     return view('welcome');
-// });
